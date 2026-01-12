70331b3c744ad9c146fe9ad75960222badbfa064
Lachlan Hunter <lhunter50@RRC.CA>
2026-01-12 17:36:42 -0600

    Start serializers

--- backend/catalog/serializers.py
+++ backend/catalog/serializers.py
@@ -1,1 +1,10 @@
 from rest_framework import serializers
+from .models import BaseItem, UniqueItem
+
+class BaseItemSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = BaseItem
+    fields = ["id", "name", "item_class", "slot"]
+
+class UniqueItemListSerializer(serializers.ModelSerializer):
+  
