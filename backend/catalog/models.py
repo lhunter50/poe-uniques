981f1a29bc0b6194f712a91154576b905f7b959e
Lachlan Hunter <lhunter50@RRC.CA>
2026-01-12 17:29:40 -0600

    Create BaseItem model and created serializers file

--- backend/catalog/models.py
+++ backend/catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class BaseItem(models.Model):
+  class ItemClass(models.TextChoices):
+    WEAPON = "weapon", "Weapon"
+    ARMOUR = "armour", "Armour"
+    ACCESSORY = "accessory", "Accessory"
+    JEWEL = "jewel", "Jewel"
+    FLASK = "flask", "Flase"
+    OTHER = "other", "Other"
+
+  name = models.CharField(max_length=200, unique=True)
+  item_class = models.CharField(max_length=20, choices=ItemClass.choices, default=ItemClass.OTHER)
+  slot = models.FloatField(max_length=100, blank=True, default="") #eg Ring, Helmet, Bow, etc.
+
+  def __str__(self):
+    return self.name
