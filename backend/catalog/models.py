e472221843fc3b4e618a334b22d3b428d55ec873
Lachlan Hunter <lhunter50@RRC.CA>
2026-01-12 17:33:33 -0600

    Created UniqueItems model with baseitem FK

--- backend/catalog/models.py
+++ backend/catalog/models.py
@@ -15,3 +15,18 @@ class BaseItem(models.Model):
 
   def __str__(self):
     return self.name
+
+class UniqueItem(models.Model):
+  name = models.CharField(max_length=200, unique=True)
+  base_item = models.ForeignKey(BaseItem, on_delete=models.PROTECT, related_name="uniques")
+  
+  required_level = models.PositiveIntegerField(null=True, blank=True)
+  image_url = models.URLField(blank=True, default="")
+
+  flavour_text = models.TextField(blank=True, default="")
+  raw_mods = models.TextField(blank=True, default="")
+
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.name
