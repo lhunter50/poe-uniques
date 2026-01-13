04dd9fd0d7d9e6bb329e80d2ba312987d7745345
Lachlan Hunter <lhunter50@RRC.CA>
2026-01-12 19:18:22 -0600

    finishing initial views for now, need to create urls now

--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -13,4 +13,22 @@ class BaseItemViewSet(viewsets.ReadOnlyModelViewSet):
   serializer_class = BaseItemSerializer
   filter_backends = [filters.SearchFilter]
   search_fields = ["name", "slot", "item_class"]
-  
+
+class UniqueItemViewSet(viewsets.ReadOnlyModelViewSet):
+  queryset = UniqueItem.objects.select_related("base_item").all().order_by("name")
+  filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+  filterset_fields = {
+    "base_item": ["exact"],
+    "required_level": ["exact", "gte", "lte"],
+    "base_item__slot": ["exact"],
+    "base_item__item_class": ["exact"],
+  }
+
+  search_fields = ["name", "required_level", "created_at"]
+  ordering = ["name"]
+
+  def get_serializer_class(self):
+    if self.action == "retrieve":
+      return UniqueItemDetailSerializer
+    return UniqueItemListSerializer
