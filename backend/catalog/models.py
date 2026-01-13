64ace1391e36fbceb6d295c93338a01737f2e3ea
Lachlan Hunter <lhunter50@RRC.CA>
2026-01-12 19:45:01 -0600

    Added league tracking presence

--- backend/catalog/models.py
+++ backend/catalog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+
 
 class BaseItem(models.Model):
   class ItemClass(models.TextChoices):
@@ -30,3 +32,49 @@ class UniqueItem(models.Model):
 
   def __str__(self):
     return self.name
+  
+class League(models.Model):
+  """
+  A league name as used by poe.ninja (e.g: Settlers, Affliction, etc.)
+  """
+  name = models.CharField(max_length=100, unique=True)
+  is_active = models.BooleanField(default=False)
+
+  created_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.name
+  
+class UniqueItemLeaguePresence(models.Model):
+  """
+  Marks that a Unique Item was observed in a given League
+  """
+
+  unique_item = models.ForeignKey(
+    "UniqueItem",
+    on_delete=models.CASCADE,
+    related_name="league_presence"
+  )
+  league = models.ForeignKey(
+    League,
+    on_delete=models.CASCADE,
+    related_name="unique_presence"
+  )
+
+  first_seen_at = models.DateField(default=timezone.now)
+  last_seen_at = models.DateField(default=timezone.now)
+
+  class Meta:
+    constraints = [
+      models.UniqueConstraint(
+        fields=["unique_item", "league"],
+        name="uniq_uniqueitem_league"
+      )
+    ]
+    indexes = [
+      models.Index(fields=["league", "last_seen_at"]),
+      models.Index(fields=["unique_item", "league"])
+    ]
+  
+  def __str__(self):
+    return f"{self.unique_item} @ {self.league}"
