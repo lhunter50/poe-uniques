c306f8832192980ba4cd86d3e00deeb8e70f2380
Lachlan Hunter <lhunter50@RRC.CA>
2026-02-11 16:49:17 -0600

    initial Types

---
+++ poe-uniques-ui/src/app/lib/types.ts
@@ -0,0 +1,32 @@
+export type BaseItem = {
+  id: number;
+  name: string;
+  item_class: string;
+  slot: string;
+}
+
+export type UniqueItem = {
+  id: number;
+  name: string;
+  required_level: number | null;
+  image_url: string;
+  base_item: BaseItem;
+
+  chaos_value: string | null;
+  divine_value: string | null;
+  listing_count: number | null;
+}
+
+export type Paginated<T> = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: T[];
+}
+
+export type UniqueQuery = { 
+  page?: string;
+  search?: string;
+  ordering?: string;
+  league?: string;
+}
