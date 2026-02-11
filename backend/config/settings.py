091e66b4401616cfc305ff0269e9a088263b5f7d
Lachlan Hunter <lhunter50@RRC.CA>
2026-02-11 15:52:48 -0600

    had to change ports for postgres, able to connect now

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -76,12 +76,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "poe_uniques",
+        "USER": "poe",
+        "PASSWORD": "poe",
+        "HOST": "127.0.0.1",
+        "PORT": "5433",
+        "CONN_MAX_AGE": 60,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
